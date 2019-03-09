fdca3756f0d9863c1f95c7e1b18ef80a17fad5c2
Steven Galvez <stikgalvez@hotmail.com>
2019-03-09 08:38:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Geo from './components/Geo';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Geo />
+    );
+  }
+}
+
+export default App;
